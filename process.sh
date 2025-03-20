a4b3735cbd42fe0878e047557c4b77297050ec7b
Colin <colin.diesh@gmail.com>
2025-03-20 01:39:07 -0400

    Misc

--- process.sh
+++ process.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-mkdir DNA_transposon_fasta
+mkdir -p DNA_transposon_fasta
 node tableify.ts | while read bed twobit accession; do
   echo $bed $twobit $accession
-  twoBitToFa $twobit $(basename $twobit).fa
-  samtools faidx $(basename $twobit).fa
+  wget -nc -q -P genomes $twobit
+  twoBitToFa genomes/$(basename twobit) genomes/$(basename $twobit).fa
+  samtools faidx genomes/$(basename $twobit).fa
 
-  bedtools slop -i DNA_transposons/$bed -g $(basename $twobit).fa.fai -l 2000 -r 2000 | gzip >DNA_transposon_fasta/slop.$bed.gz
-  bedtools getfasta -bed DNA_transposon_fasta/slop.$bed.gz -fi $(basename $twobit).fa -name+ | bgzip >DNA_transposon_fasta/$accession.dna_transposons.fa.gz
+  bedtools slop -i DNA_transposons/$bed -g genomes/$(basename $twobit).fa.fai -b 2000 | gzip >DNA_transposons/slop.$bed.gz
 
-  rm -f $(basename $twobit).fa*
+  ## output slop'd fa
+  bedtools getfasta -bed DNA_transposons/slop.$bed.gz -fi genomes/$(basename $twobit).fa -name+ | bgzip >DNA_transposon_fasta/$accession.dna_transposons.slop.fa.gz
+
+  ## output non-slop'd fa
+  bedtools getfasta -bed DNA_transposons/$bed -fi $(basename $twobit).fa -name+ | bgzip >DNA_transposon_fasta/$accession.dna_transposons.fa.gz
+
 done
